29874dcdbdf23b259bd8c89ae1e3fdf9c3915e43
Tomas.Beran <Tomas.Beran@gmail.com>
2024-06-13 21:28:16 +0000

    modified:   config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
-SECTION = 'RFID'
-BTMIX_FILE = 'DB File'
 DEVEL = 0
 DEVEL = 1 # 1 for local development. Must be commented before deploy to photobooth
 PLUGIN_ROOT = '/home/pi/pibooth/pibooth-rfid/'
+
+
 if DEVEL:
     PLUGIN_ROOT = './' 
 DATA_ROOT = PLUGIN_ROOT + 'data/' 
@@ -11,3 +11,5 @@ BADGES_IMG_FOLDER = 'Badge Folder'
 BADGES_IMG_FOLDER_DEFAULT = DATA_ROOT + 'badges/' # for local development
 BADGES_DEFAULT_IMG = 'Default image'
 BADGES_DEFAULT_IMG_FILE = PLUGIN_ROOT + 'nobody.jpg'
+SECTION = 'RFID'
+BTMIX_FILE = 'DB File'
